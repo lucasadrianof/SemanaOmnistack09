5b6c4ca0164bd00134c42f75b655c6056e04345a
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2019-10-03 23:04:07 -0300

    Processo de Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+     
+    const response = await api.post('/sessions', { email });
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnc"/>
@@ -13,9 +25,15 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre talentos para sua <strong>empresa</strong>
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL*</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail"/>
+          <input 
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
           <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
