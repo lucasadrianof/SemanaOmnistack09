12a49c1a097094f006fbe138dd937cb6b60036eb
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2019-10-04 19:08:27 -0300

    Rota de Arquivos Estáticos

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const routes = require("./routes");
 
@@ -17,5 +18,6 @@ mongoose.connect(
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3001);
