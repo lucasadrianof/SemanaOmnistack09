368e9b527b75d97d0804b3efd421c10dfb860d6a
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2019-10-06 13:47:24 -0300

    Listagem de Spots - Mobile

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,47 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  AsyncStorage,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Image
+} from "react-native";
+
+import SpotList from "../components/SpotList";
+import logo from "../../assets/logo.png";
 
 export default function List() {
-  return <View />;
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(chosenTechs => {
+      const arrayTechs = chosenTechs.split(",").map(tech => tech.trim());
+
+      setTechs(arrayTechs);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 30
+  }
+});
